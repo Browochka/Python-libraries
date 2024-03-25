5f1d8ec5a4a59269e2dcec49492a82888d8c8240
Browka <okkarunko@gmail.com>
2024-03-25 08:09:32 +0300

    Numpy start

---
+++ Numpy/main.py
@@ -0,0 +1,24 @@
+from typing import Any
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+
+#a=np.arange(0,26,5)
+
+#e=np.array([2,3,4,6,9,1])
+
+#a=np.sqrt(a) #корень
+
+#b=np.linspace(0,50,5)
+
+#c=np.random.random((5))
+
+#d=(10-5)*np.random.random_sample((5)) + 5  #числа в диапазоне [5;10]
+
+#f=np.random.randint(5,10,8)
+
+# np.insert -вставить значение
+
+j=np.array([1,2,3,4,5,10,15,3,2,11])
+print(j[(j>5) & (j < 12)])
+
